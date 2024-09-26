aca888de71b9a110e90ab9faee2b56de2dcaa83c
Andrew538 <andrew742183@mail.ru>
2024-09-26 15:26:43 +0500

    edd registration

--- src/components/pages/AdminPanel/AdminPanel.jsx
+++ src/components/pages/AdminPanel/AdminPanel.jsx
@@ -1,40 +1,37 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 // import { registration } from '../../../http/userAPI';
-// import { Context } from '../../../index';
+import { Context } from '../../../index';
+import { registration } from '../../http/userAPI';
 
 function AdminPanel() {
-  // const {user} = useContext(Context)
-  // const [email, setEmail] = useState('')
-  // const [password, setPassword] = useState('')
-  // const signIn = async () => {
-  //   let data;
-  //   try {
-  //     const data  = await registration(email, password)
-  //     // console.log(response)
-  //     user.setUser(user)
-  //     user.setIsAuth(true)
-  //   } catch (err) {
-  //     if (err.response.status === 404) {
-  //         // alert(err.response.error);
-  //     }
-  // }
+  const {user} = useContext(Context)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const signIn = async () => {
+    let data;
+    try {
+      const data  = await registration(email, password)
+      // console.log(response)
+      user.setUser(user)
+      user.setIsAuth(true)
+    } catch (err) {
+      if (err.response.status === 404) {
+          // alert(err.response.error);
+      }
+  }
     
-    // catch (e) {
-    //   // console.log(e.response.data.message)
-    //   expect(e.message).toEqual('Error message');
-
-    // }
-
-   
  
    
-  // }
+   
+  }
 
 
   return (
    <div>
-    {/* //  <input placeholder='Логин' value={email} onChange={e => setEmail(e.target.value)}/>
-    //  <input placeholder='пароль' value={password} onChange={e => setPassword(e.target.value)}/>  */}
+     <input placeholder='Логин' value={email} onChange={e => setEmail(e.target.value)}/>
+     <input placeholder='пароль' value={password} onChange={e => setPassword(e.target.value)}/> 
+     <button onClick={signIn}>Создать</button>
+
     <h1>Админ</h1>
     </div>
   )
