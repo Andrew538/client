a8af766f006e9a06c0e64f77abed575ac223b46a
Andrew538 <andrew742183@mail.ru>
2024-10-03 21:38:44 +0500

    m

--- src/components/http/index.js
+++ src/components/http/index.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const $host = axios.create({
     // baseURL: 'https://localhost:5000/'
-    baseURL: 'https://lk-opt.store:5000/'
-    // proxy: {
-    //     host: '45.146.165.155:5000',
-    //     port: 80,
-    //     protocol: 'http',
-    // }
-// 
+    // baseURL: 'https://lk-opt.store:5000/'
+    proxy: {
+        host: '45.146.165.155:5000',
+        port: 80,
+        protocol: 'http',
+    }
+
 })
 
 const $authHost = axios.create({
