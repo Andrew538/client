19a9debcc6cc17176b5442a8bfc74d712516a1d2
Andrew538 <andrew742183@mail.ru>
2024-10-27 14:42:47 +0500

    new rout

--- src/components/http/guaranteeAPI.js
+++ src/components/http/guaranteeAPI.js
@@ -2,14 +2,14 @@ import { $authHost, $host } from "./index";
 import { jwtDecode } from "jwt-decode";
 
 export const createRecorder = async (date, client, manager, product, releaseDate, result) => {
-    const {data} = await $authHost.post('api/examination', {date, client, manager, product, releaseDate, result})
+    const {data} = await $authHost.post('api/examination/newentry', {date, client, manager, product, releaseDate, result})
    return data
 
 }
 
 
 export const fetchExam = async (date, client, manager, product, releaseDate, result) => {
-    const {data} = await $authHost.get('api/examination', {params: {
+    const {data} = await $authHost.get('api/examination/getall', {params: {
         date, client, manager, product, releaseDate, result
     }})
     console.log(data)
@@ -18,7 +18,7 @@ export const fetchExam = async (date, client, manager, product, releaseDate, res
 }
 
 export const delExam = async (id) => {
-    const {} = await $authHost.delete('api/examination/', {data: id})
+    const {} = await $authHost.delete('api/examination/del', {data: id})
   
 
 }
--- src/components/http/index.js
+++ src/components/http/index.js
@@ -1,30 +1,30 @@
 import axios from "axios";
 
-// const $host = axios.create({
-//     baseURL: 'http://localhost:5000/'
-// })
-
 const $host = axios.create({
- 
-    proxy: {
-        host: '45.146.165.155:5000',
-        port: 80,
-        protocol: 'http',
-    }
-
+    baseURL: 'http://localhost:5000/'
 })
 
-// const $authHost = axios.create({
-//     baseURL: 'http://localhost:5000/'
+// const $host = axios.create({
+ 
+//     proxy: {
+//         host: '45.146.165.155:5000',
+//         port: 80,
+//         protocol: 'http',
+//     }
+
 // })
 
 const $authHost = axios.create({
-    proxy: {
-        host: '45.146.165.155:5000',
-        port: 80,
-        protocol: 'http',
-    }
+    baseURL: 'http://localhost:5000/'
 })
+
+// const $authHost = axios.create({
+//     proxy: {
+//         host: '45.146.165.155:5000',
+//         port: 80,
+//         protocol: 'http',
+//     }
+// })
 
 const authInterceptor = config => {
     config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
