2b16f7bcc4dbef03dbca3b76fd360848379c3067
Andrew538 <andre742183@mail.ru>
2024-12-04 20:34:58 +0500

    add proxy

--- src/components/http/index.js
+++ src/components/http/index.js
@@ -1,32 +1,32 @@
 import axios from "axios";
 
-const $host = axios.create({
-    baseURL: 'http://localhost:5000/'
-})
-
-const $authHost = axios.create({
-    baseURL: 'http://localhost:5000/'
-})
-
-
 // const $host = axios.create({
- 
-//     proxy: {
-//         host: '45.146.165.155:5000',
-//         port: 80,
-//         protocol: 'http',
-//     }
-
+//     baseURL: 'http://localhost:5000/'
 // })
-
 
 // const $authHost = axios.create({
-//     proxy: {
-//         host: '45.146.165.155:5000',
-//         port: 80,
-//         protocol: 'http',
-//     }
+//     baseURL: 'http://localhost:5000/'
 // })
+
+
+const $host = axios.create({
+ 
+    proxy: {
+        host: '45.146.165.155:5000',
+        port: 80,
+        protocol: 'http',
+    }
+
+})
+
+
+const $authHost = axios.create({
+    proxy: {
+        host: '45.146.165.155:5000',
+        port: 80,
+        protocol: 'http',
+    }
+})
 
 const authInterceptor = config => {
    
