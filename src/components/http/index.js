51bec2324c9a3aa014df9d9e075ee1c659d96c74
Andrew538 <andre742183@mail.ru>
2025-02-24 21:18:18 +0500

    new

--- src/components/http/index.js
+++ src/components/http/index.js
@@ -1,33 +1,33 @@
 import axios from "axios";
 
-const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
-   
-})
-
-const $authHost = axios.create({
-    baseURL: 'http://localhost:5000/'
-})
-
-
 // const $host = axios.create({
- 
-//     proxy: {
-//         host: '45.146.165.155:5000',
-//         port: 80,
-//         protocol: 'http',
-//     }
-
+//     baseURL: process.env.REACT_APP_API_URL
+   
 // })
-
 
 // const $authHost = axios.create({
-//     proxy: {
-//         host: '45.146.165.155:5000',
-//         port: 80,
-//         protocol: 'http',
-//     }
+//     baseURL: 'http://localhost:5000/'
 // })
+
+
+const $host = axios.create({
+ 
+    proxy: {
+        host: '45.146.165.155:5000',
+        port: 80,
+        protocol: 'http',
+    }
+
+})
+
+
+const $authHost = axios.create({
+    proxy: {
+        host: '45.146.165.155:5000',
+        port: 80,
+        protocol: 'http',
+    }
+})
 
 const authInterceptor = config => {
    
