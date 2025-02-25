36cdbd910d0f1e6aeced9e8433e792d6de959748
Andrew538 <andre742183@mail.ru>
2025-02-25 19:38:37 +0500

    new

--- src/components/WarrantyVerificationSteps/NewCheck.jsx
+++ src/components/WarrantyVerificationSteps/NewCheck.jsx
@@ -85,7 +85,7 @@ const  NewCheck = observer(() => {
       <WarrantyTableHeader/>
       
         <ol className={classes.list}>
-            {examination.examination.map((item) =>    
+            {sortedList.map((item) =>    
                 <li className={classes.item} key={item.id}>
                 <div className={classes.item__box}>
                   <div className={classes.table}>    
--- src/components/pages/Guarantee/Guarantee.jsx
+++ src/components/pages/Guarantee/Guarantee.jsx
@@ -19,7 +19,7 @@ const  Guarantee = observer(() => {
         <NavLink className={classNames(classes.nav__link)}  to='ready'>Готов к отправке клиенту</NavLink>
         <NavLink className={classNames(classes.nav__link)}  to='arhive'>Архив</NavLink>
       </div>
-          <Outlet/>
+          {/* <Outlet/> */}
     </div>
   )
 })
