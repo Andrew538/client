8edaa8d8d64b5305db34efbff4bef7c708234a9b
Andrew538 <andre742183@mail.ru>
2025-04-06 11:03:51 +0500

    auth 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, useNavigate} from 'react-router-dom'
+import {BrowserRouter, } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import UserStore from './components/store/UserStore';
@@ -12,51 +12,51 @@ import { check } from './components/http/userAPI';
 
 
 export const Context = createContext(null)
-  const {users}  = useContext(Context)
+  // const {users}  = useContext(Context)
 
- const navigate = useNavigate()
-    const singout = useAuth()
+//  const navigate = useNavigate()
+//     const singout = useAuth()
 
- useEffect(() => {
+//  useEffect(() => {
      
-        // setUser(localStorage.getItem('token'))
-        try {
-            if(localStorage.getItem('token')) {
-                check()
-                .then(data => {
+//         // setUser(localStorage.getItem('token'))
+//         try {
+//             if(localStorage.getItem('token')) {
+//                 check()
+//                 .then(data => {
                    
-                    if(localStorage.getItem('token') && data) {
-                        users.setUser(true)
-                        users.setIsAuth(true)
-                        users.setRole(data.role)
-                        users.setEmail(data.email)
-                    } else if(localStorage.getItem(' ', ) && !data) {
-                        localStorage.clear();
-                        navigate('/', {replace: true})
-                        singout(()=> 
-                            navigate('/', {replace: true})
-                        )
-                    }
-                }) 
-                // .catch(function(error) {
-                //     console.log(error.response.status)
-                //     if(error.response.status === 401) {
-                //     navigate('/home', {replace: true})
+//                     if(localStorage.getItem('token') && data) {
+//                         users.setUser(true)
+//                         users.setIsAuth(true)
+//                         users.setRole(data.role)
+//                         users.setEmail(data.email)
+//                     } else if(localStorage.getItem(' ', ) && !data) {
+//                         localStorage.clear();
+//                         navigate('/', {replace: true})
+//                         singout(()=> 
+//                             navigate('/', {replace: true})
+//                         )
+//                     }
+//                 }) 
+//                 // .catch(function(error) {
+//                 //     console.log(error.response.status)
+//                 //     if(error.response.status === 401) {
+//                 //     navigate('/home', {replace: true})
 
-                //     }
-                // })
-            } 
+//                 //     }
+//                 // })
+//             } 
           
-        } catch (error) {
-            if(error.error) {
-              navigate('/home', {replace: true})
+//         } catch (error) {
+//             if(error.error) {
+//               navigate('/home', {replace: true})
 
-            }
-            console.log(error)
-            console.log(error)
-        }
+//             }
+//             console.log(error)
+//             console.log(error)
+//         }
        
-    }, [])
+//     }, [])
 
 
 
