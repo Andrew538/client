ae154ac61549c45366f9bbc6be02701d8f660606
Andrew538 <andre742183@mail.ru>
2025-05-31 11:02:06 +0500

    delete export function

--- src/components/UI/ModalUpdate/ModalUpdate.jsx
+++ src/components/UI/ModalUpdate/ModalUpdate.jsx
@@ -29,62 +29,62 @@ const ModalUpdate = observer(({show, onHide,  props}) => {
 
     const id = +props;
 
-    // function fullUdate() {
-    //   function resultUpdate() {
-    //     if (result === "") {
-    //       const result = addRec.result;
-    //       updateRecord(id, result, statusExam);
-    //     } else if (result.length) {
-    //       updateRecord(id, result, statusExam);
-    //     }
-    //   }
-    //   resultUpdate();
-    //   function numberReturnDocumentUdate() {
-    //     if (numberReturnDocument === "") {
-    //       const numberReturnDocument = addRec.numberReturnDocument;
-    //       updateNumberReturnDocument(id, numberReturnDocument, statusExam);
-    //     } else {
-    //       updateNumberReturnDocument(id, numberReturnDocument, statusExam);
-    //     }
-    //   }
-    //   numberReturnDocumentUdate();
-    //   function plantDocumentNumberUpdate() {
-    //     if (plantDocumentNumber === "") {
-    //       const plantDocumentNumber = addRec.plantDocumentNumbert;
-    //       updatePlantDocumentNumber(id, plantDocumentNumber, statusExam);
-    //     } else {
-    //       updatePlantDocumentNumber(id, plantDocumentNumber, statusExam);
-    //     }
-    //   }
-    //   plantDocumentNumberUpdate();
+    function fullUdate() {
+      function resultUpdate() {
+        if (result === "") {
+          const result = addRec.result;
+          updateRecord(id, result, statusExam);
+        } else if (result.length) {
+          updateRecord(id, result, statusExam);
+        }
+      }
+      resultUpdate();
+      function numberReturnDocumentUdate() {
+        if (numberReturnDocument === "") {
+          const numberReturnDocument = addRec.numberReturnDocument;
+          updateNumberReturnDocument(id, numberReturnDocument, statusExam);
+        } else {
+          updateNumberReturnDocument(id, numberReturnDocument, statusExam);
+        }
+      }
+      numberReturnDocumentUdate();
+      function plantDocumentNumberUpdate() {
+        if (plantDocumentNumber === "") {
+          const plantDocumentNumber = addRec.plantDocumentNumbert;
+          updatePlantDocumentNumber(id, plantDocumentNumber, statusExam);
+        } else {
+          updatePlantDocumentNumber(id, plantDocumentNumber, statusExam);
+        }
+      }
+      plantDocumentNumberUpdate();
 
-    //   function movingToDefectWarehouseUpdate() {
-    //     if (movingToDefectWarehouse === "") {
-    //       const movingToDefectWarehouse = addRec.movingToDefectWarehouse;
-    //       updateMovingToDefectWarehouse(
-    //         id,
-    //         movingToDefectWarehouse,
-    //         statusExam
-    //       );
-    //     } else {
-    //       updateMovingToDefectWarehouse(
-    //         id,
-    //         movingToDefectWarehouse,
-    //         statusExam
-    //       );
-    //     }
-    //   }
-    //   movingToDefectWarehouseUpdate();
-    //   function ReleaseDateUpdate() {
-    //     if (releaseDate === "") {
-    //       const releaseDate = addRec.releaseDate;
-    //       updateReleaseDate(id, releaseDate, statusExam);
-    //     } else {
-    //       updateReleaseDate(id, releaseDate, statusExam);
-    //     }
-    //   }
-    //   ReleaseDateUpdate();
-    // }
+      function movingToDefectWarehouseUpdate() {
+        if (movingToDefectWarehouse === "") {
+          const movingToDefectWarehouse = addRec.movingToDefectWarehouse;
+          updateMovingToDefectWarehouse(
+            id,
+            movingToDefectWarehouse,
+            statusExam
+          );
+        } else {
+          updateMovingToDefectWarehouse(
+            id,
+            movingToDefectWarehouse,
+            statusExam
+          );
+        }
+      }
+      movingToDefectWarehouseUpdate();
+      function ReleaseDateUpdate() {
+        if (releaseDate === "") {
+          const releaseDate = addRec.releaseDate;
+          updateReleaseDate(id, releaseDate, statusExam);
+        } else {
+          updateReleaseDate(id, releaseDate, statusExam);
+        }
+      }
+      ReleaseDateUpdate();
+    }
            
 
     const options = [
@@ -133,7 +133,7 @@ const ModalUpdate = observer(({show, onHide,  props}) => {
       
        try {
    
-        fullUdate(id, numberReturnDocument,  plantDocumentNumber, movingToDefectWarehouse, releaseDate, result, statusExam, addRec)
+        fullUdate()
         Update()
         setDate("");
         setclient("");
