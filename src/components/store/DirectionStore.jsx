70f0c2fdf54ab03accbb1b0a4c44acb751da975a
Andrew538 <andre742183@mail.ru>
2025-07-06 20:17:58 +0500

    del sort

--- src/components/store/DirectionStore.jsx
+++ src/components/store/DirectionStore.jsx
@@ -14,11 +14,13 @@ export default class DirectionStore {
   }
 
   SetAllDirection(alldirection) {
-    this._alldirection = alldirection.sort((a, b) => a.region.toLowerCase() > b.region.toLowerCase() ? 1 : -1);
+    // this._alldirection = alldirection.sort((a, b) => a.region.toLowerCase() > b.region.toLowerCase() ? 1 : -1);
+    this._alldirection = alldirection
   }
 
     SetAllCity(allcity) {
-    this._allcity = allcity.sort((a, b) => a.city.toLowerCase() > b.city.toLowerCase() ? 1 : -1);
+    // this._allcity = allcity.sort((a, b) => a.city.toLowerCase() > b.city.toLowerCase() ? 1 : -1);
+    this._allcity = allcity
   }
 
 
